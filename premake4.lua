ae596e2eec8a83f42354766e3f417514db631622
Oliver Schneider <oliver@assarbad.net>
2016-01-12 18:27:22 +0000

    Using -a to append a SHA-256-hashed signature as well during code-signing
    --HG--
    branch : WDS-build

--- premake4.lua
+++ premake4.lua
@@ -134,7 +134,7 @@ end
 			files       { "src/host/premake4.rc" }
 
 		configuration {"windows", "Publish"}
-			postbuildcommands { 'ollisign.cmd "$(TargetPath)" "https://bitbucket.org/windirstat/premake-stable" "premake4"' }
+			postbuildcommands { 'ollisign.cmd -a "$(TargetPath)" "https://bitbucket.org/windirstat/premake-stable" "premake4"' }
 			defines     "HAVE_HGTIP"
 
 		configuration "linux or bsd"
